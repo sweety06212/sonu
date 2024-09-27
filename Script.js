19a267b2f47ab8c89b8b875fff50f0189456e071
Sonu2324 <hamshinichhabhaiya4@gmail.com>
2024-09-27 22:26:27 +0530

    hehe

---
+++ Script.js
@@ -0,0 +1,10 @@
+function vote(){
+    var name = document.getElementById("name").value;
+
+    var age = document.getElementById("Age").value;
+
+    var pass = 100*Math.random();
+    var result = document.getElementById("Result")
+
+
+}
